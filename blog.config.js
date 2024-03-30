65883736bf2614a51d0c32beb7bec8af3abf1fa7
sobercentury <sobercentury@gmail.com>
2024-03-30 19:23:14 +0800

    更改基础信息

--- blog.config.js
+++ blog.config.js
@@ -42,9 +42,9 @@ const BLOG = {
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   notionDomain: 'zegqan.notion.site',
   telegramToken: process.env.TELEGRAM_TOKEN, // The token of your Telegram bot
-  telegramChatId: '263895784', // The chat id of your Telegram bot
-  telegramChannelUrl: 'https://channel.zuolan.me/', // The link of your Telegram channel
-  telegramChannelName: 'zuolan_me', // The name of your Telegram channel
+  telegramChatId: '817871929', // The chat id of your Telegram bot
+  telegramChannelUrl: 'https://t.me/boojun_com', // The link of your Telegram channel
+  telegramChannelName: 'boojun_com', // The name of your Telegram channel
   craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
